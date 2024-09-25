f4b51da96c2d9669935e9037d4a6ac796f2d3818
Silvia Aguilar <silviaeaguilar@gmail.com>
2024-09-25 10:12:49 -0300

    redsys/stripeTests

--- cypress/e2e/helpers/setPaymentProvider.cy.js
+++ cypress/e2e/helpers/setPaymentProvider.cy.js
@@ -69,12 +69,6 @@ export class setPaymentProvider {
             }
         });
 
-        cy.get('#pp-select').select('stripe')
-        cy.get('[name="pp[stripe_env]"]').type('dev')
-        cy.get('[name="pp[stripe_public_key]"]').type('pk_test_51PidtGRwak0wZyPnr995GgXZA5WX8pr1xDUDM1FXruj2tmEGzECury8DIUXSZAFlmBX2pBPUXANpkFf5SfG83xPW00KqLhUA34')
-        cy.get('[name="pp[stripe_secret_key]"]').type('sk_test_51PidtGRwak0wZyPnqLpp5KvnA8KiGdn2YZpu126DbHgjtBsxtgmoFJ2JF8lotocBbD0yDVlYqy70zz6wZNYJOFy8007khW8zgB')
-        cy.get('[name="pp[stripe_secret_webhook]"]').type('whsec_Xig6jQsdgYScENssffQOM4Tp0K1vS0su')
-        cy.get('.checkbox-group > [value="1"]').check()
-        cy.get('#add-pp > [style="display: inline-block"]').click()
+
     }
 }
